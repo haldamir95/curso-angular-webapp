0bc056f7120cc3a4276cb92dbf6642e86ac843bb
haldamir95 <haldamir.95@gmail.com>
2021-08-29 23:29:36 -0600

    Agregar prducto con IMAGEN

--- src/app/components/producto-add.component.ts
+++ src/app/components/producto-add.component.ts
@@ -13,6 +13,9 @@ import { ProductoService } from "../services/producto.service";
 import { Producto } from "../models/producto";
 
 
+//importar variables de entorno GLOBAL
+import { GLOBAL } from "../services/global";
+
 
 //DECORADOR DEL COMPONENTE
 @Component(
@@ -31,7 +34,12 @@ export class ProductoAddComponent{
     public titulo!:string;
     public producto!:Producto;
 
-    constructor(){
+    constructor(
+        //Agregar las propiedades privadas para utilizar el servicio del producto
+        private _productoService : ProductoService,
+        private _route: ActivatedRoute,
+        private _router: Router
+    ){
         this.titulo = 'Crear un Nuevo Producto';
         this.producto = new Producto(0,"","",0,"");
     }
@@ -43,5 +51,60 @@ export class ProductoAddComponent{
 
     onSubmit(){
         console.log(this.producto);
+
+        //validar que se quiera subir un producto
+        if(this.filesToUpload.length >= 1){
+            //INTENTAR SUBIR EL ARCHIVO 
+            this._productoService.makeFileRequest(GLOBAL.url+'/upload-file',[],this.filesToUpload).then(
+                (result)=>{
+                    //el result viene como un objeto desconocido, lo pasamos a jsonstringify
+                    let cadena = JSON.stringify(result);
+                    //ahora que lo tenemos como cadena lo regresamos a json
+                    let json = JSON.parse(cadena);
+                    console.log(json);
+                    //ahora el result esta en la var json y ya podemos acceder a sus propiedades
+                    this.producto.imagen = json.filename;
+                    //CUANDO SE HAYA SUBIDO CORRECTAMENTE EL ARCHIVO, SE GUARDA LA INFORMACION EN LA BASE DE DATOS
+                    this.saveProducto();
+                },
+                (error)=>{
+                    console.log(error);
+                }
+            );
+        }else{
+            this.saveProducto();
+        }
+        
+    }
+
+
+    saveProducto(){
+        //utilizando el servicio mandar como parametro el producto
+        //utilizar el metodo subscribe de los Observable para utilizar los resultados callback response y error
+        this._productoService.addProducto(this.producto).subscribe(
+            response => {
+                if(response.code == 200){
+                    this._router.navigate(['/productos']);
+                }else{
+                    console.log(response);
+                }
+            },
+            error => {
+                console.log(<any>error);
+            }
+        );
+    }
+
+
+
+    //variables globales para subir archivos
+    public filesToUpload!:any;
+    public resultUpload!:any;
+    fileChangeEvent(fileInput: any){
+
+        //obtener el input file y monta el archivo en un array
+        this.filesToUpload = <Array<File>>fileInput.target.files;
+        
+        console.log(this.filesToUpload);
     }
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -2,7 +2,7 @@
 import { Injectable } from "@angular/core";
 
 //importaciones para los servicios con protocolos http
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, XhrFactory } from "@angular/common/http";
 import { HttpResponse } from "@angular/common/http";
 import { HttpHeaders } from "@angular/common/http";
 
@@ -34,6 +34,52 @@ export class ProductoService{
 
     getProductos(){
         return this._http.get(this.url+'/productos').pipe(map(res => JSON.parse(JSON.stringify(res))));
+    }
+
+    addProducto(producto: Producto){
+        //Convertir el objeto producto en JSON para enviarlo al API
+        let json = JSON.stringify(producto);
+        //Crear la variable json que se envia al servicio
+        let params = 'json='+json;
+        //definir header para definir como nuestro backend procesa la informacion que llega por post
+        //tomar en cuenta que se utiliza HttpHeaders en esta version de Angular
+        let headers = new HttpHeaders().set('Content-Type','application/x-www-form-urlencoded');
+        //hacer la peticion y devolver el resultado
+        //transformar el res a un string y despues a un json.parser para que pueda ser utilizado por JavaScript
+        return this._http.post(this.url+'/productos', params, {headers: headers}).pipe(map(res => JSON.parse(JSON.stringify(res))));
+    }
+
+
+    makeFileRequest(url:string, params: Array<string>, files: Array<File>){
+
+        return new Promise((resolve, reject)=>{
+            //variable para simular un formulario
+            var formData: any = new FormData();
+            //variable para tener disponible las peticiones AJAX
+            var xhr = new XMLHttpRequest();
+
+            //foor para recorer el array de archivos
+            for(var i =0; i<files.length; i++){
+                formData.append('uploads[]', files[i], files[i].name);
+            }
+
+            //cuando la peticion este preparada hacer una funcion anonima
+            xhr.onreadystatechange = function(){
+                if(xhr.readyState == 4){
+                    if(xhr.status == 200){
+                        resolve(JSON.parse(xhr.response))
+                    }else{
+                        reject(xhr.response);
+                    }
+                }
+            };
+
+            //abrir la peticion
+            xhr.open("POST", url, true);
+            //enviar el formulario
+            xhr.send(formData);
+
+        });
     }
 
 }
